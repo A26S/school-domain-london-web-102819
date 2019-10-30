6f311c5e3e6ac865e67d8a3be957b470db0b8962
Ali Saidik <saida130.210@gmail.com>
2019-10-30 12:20:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+  def initialize(roster)
+    @roster = roster
+  end 
+  def roster=(roster)
+    @roster = roster
+  end 
+  def roster
+    @roster = {}
+  end 
+  def add_student(student, grade)
+    roster[grade] = []
+    roster[grade] << student
+  end 
+  def add_student(student, grade)
+    @grade = []
+    @grade << student 
+  end 
+  def grade(student)
+    
+  end
+  def sort
+    @student.sort
+  end 
+end
